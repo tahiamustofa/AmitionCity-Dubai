ae03b7de2a1358ca89f452878e73c88df4793979
tahia <tahiamustofa@gmail.com>
2025-03-11 15:58:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import CarouselBasicExample from "./components/CarouselBasicExample/CarouselBasicExample";
+import Visa from "./components/Visa";
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <CarouselBasicExample />
+      <Visa></Visa>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
